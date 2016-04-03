b29537c4b1612465975a9b597095534da0967eef
Vijay Ananthraman <vananthraman@expedia.com>
2016-04-02 20:01:14 -0700

    updating var port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function(request,response){
 	response.sendFile(__dirname+'/index.html');
 });
 
-var port =8080;
+var port =process.env.PORT||8080;
 
 var server=app.listen(port,function(req,res){
 	console.log("catch the action at http://localhost:"+port);
